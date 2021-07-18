73ae556bc6db381fb5c3a829a8d0627c79889e1e
Pebin <petrbarton2@gmail.com>
2021-07-18 15:34:24 +0200

    add logging

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
 import json
+import logging
+import os
 
 from dateutil import parser
 from elasticsearch import Elasticsearch
@@ -9,18 +11,29 @@ app = Flask(__name__)
 
 DATA_FILE = "elektrina_data.txt"
 
-es = Elasticsearch()
+elastic_url = os.getenv("ELASTICSEARCH_URL", "localhost")
+es = Elasticsearch([elastic_url])
 
-try:
-    res = es.search(index="flash-data", body={"query": {"match_all": {}}})
-    print("Got %d Hits:" % res['hits']['total']['value'])
-except Exception as ex:
-    print(f"Could not read flash-data from elastic: {ex}")
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('ledkovac-server')
+
+
+def get_hits_in_elastic():
+    return es.search(index="flash-data", body={"query": {"match_all": {}}})
 
 
 @app.route("/")
 def index():
     return "Test"
+
+
+@app.route("/data-count")
+def data_count():
+    try:
+        hits = get_hits_in_elastic()['hits']['total']['value']
+        return f"hits in elastic - {hits}", 200
+    except Exception as ex:
+        logger.info(f"Could not read flash-data from elastic: {ex}"), 400
 
 
 @app.route('/new-data', methods=['POST'])
@@ -31,11 +44,11 @@ def new_data():
     try:
         with open(DATA_FILE, "a") as data_file:
             data_file.write(f"{date}\n")
-            result = es.index(index="flash-data", body={'id': data_id, 'timestamp': parsed_date})
-            print(f"{data_id} - {date} : {result['result']}")
+        result = es.index(index="flash-data", body={'id': data_id, 'timestamp': parsed_date})
+        logger.info(f"{data_id} - {date} : {result['result']}")
     except Exception as ex:
-        return f"error - {ex}"
-    return 'ok'
+        return f"error - {ex}", 400
+    return 'ok', 200
 
 
 if __name__ == "__main__":
